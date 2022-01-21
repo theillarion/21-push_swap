46bc1d5268072c011a6e41e5428e1a2a159d92c4
Illarion <pro100illarion@gmail.com>
2022-01-22 01:40:47 +0300

    Added new function prototypes

---
+++ includes/operations.h
@@ -0,0 +1,11 @@
+#ifndef OPERATIONS_H
+# define OPERATIONS_H
+
+# include "stack.h"
+
+void	ft_swap(t_stack	**head);
+void	ft_push_(t_stack	**src, t_stack	**dest);
+void	ft_rotate(t_stack	**head);
+void	ft_reverse_rotate(t_stack	**head);
+
+#endif
