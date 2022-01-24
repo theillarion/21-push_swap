d470f58aca34fb9af17aa3eed92b86c3ed1f2660
Illarion <pro100illarion@gmail.com>
2022-01-24 21:36:55 +0300

    Added new functions

--- srcs/main.c
+++ srcs/main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: illarion <illarion@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/23 19:25:15 by illarion          #+#    #+#             */
+/*   Updated: 2022/01/24 15:00:32 by illarion         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "operations.h"
 #include "libft.h"
 #include <stdio.h>
@@ -10,37 +22,36 @@ void	ft_error(t_stack	*stack)
 	exit(EXIT_FAILURE);
 }
 
-t_stack	*ft_validation(char	**argv)
+bool	ft_chars_is_digit(const char	*str)
 {
+	while (*str)
+		if (!ft_isdigit(*(str++)))
+			return (false);
+	return (true);
+}
+
+t_stack	*ft_fill_stack(char	**argv)
+{
+	size_t	i;
 	char	**buff;
 	t_stack	*stack;
-	//size_t	i;
-	//bool	is_digit;
 
 	while (*argv)
 	{
 		buff = NULL;
+		i = 0;
 		buff = ft_split(*(argv++), ' ');
-		while (*buff)
+		while (buff[i] != NULL)
 		{
-			//i = 0;
-			/*while ((*buff)[i])
-			{
-				is_digit = ft_isdigit((*buff)[i++]);
-				if (is_digit == false)
-					break;
-			}*/
-			//if (is_digit)
-			ft_push(&stack, ft_atoi(*buff));
-			//else
-			//	ft_error(stack);
-			free((*buff)++);
+			if (!ft_chars_is_digit(buff[i]))
+				ft_error(stack);
+			ft_push(&stack, ft_atoi(buff[i]));
+			free(buff[i++]);
 		}
 		free(buff);
 	}
 	return (stack);
 }
-
 
 void	print_stack(t_stack	*stack)
 {
@@ -55,14 +66,24 @@ void	print_stack(t_stack	*stack)
 	printf("\n");
 }
 
-int main(int argc, char	**argv)
+int	main(int argc, char	**argv)
 {
-	(void)argc;
 	t_stack	*a;
 	t_stack	*b;
-	(void)b;
 
-	a = ft_validation(argv);
-	print_stack(a);
-	exit(EXIT_SUCCESS);
+
+	(void)b;
+	(void)argc;
+	if (argc > 1)
+	{
+		a = ft_fill_stack(++argv);
+		print_stack(a);
+		exit(EXIT_SUCCESS);
+	}
+	else
+	{
+		/* ERROR */
+		write(1, "Error\n", 6);
+		exit(EXIT_FAILURE);
+	}
 }
