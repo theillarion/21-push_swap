54d56f40016580ac7e2c2a9a5a2d865e286dc8e1
Illarion <pro100illarion@gmail.com>
2022-01-26 18:56:03 +0300

    Added new prorotype functions

--- includes/vector.h
+++ includes/vector.h
@@ -4,7 +4,7 @@
 # include <stddef.h>
 # include <stdlib.h>
 # include <limits.h>
-#include <stdbool.h>
+# include <stdbool.h>
 
 typedef struct s_vector
 {
@@ -12,11 +12,16 @@ typedef struct s_vector
 	size_t	count;
 }			t_vector;
 
-void	ft_initial(t_vector	*vector);
-void 	ft_push_back(t_vector	*vector, int element);
-int		ft_get_element(const t_vector vector, size_t index);
-size_t	ft_get_count(const t_vector vector);
-void	ft_delete_vector(t_vector	*vector);
-bool	ft_sort(t_vector	*vector);
+void		ft_initial_vector(t_vector	*vector);
+void 		ft_push_back(t_vector	*vector, int element);
+int			ft_get_element(const t_vector vector, size_t index);
+size_t		ft_get_count(const t_vector vector);
+void		ft_delete_vector(t_vector	*vector);
+bool		ft_sort_vector(t_vector	*vector);
+
+bool		ft_erase_back(t_vector	*vector);
+int			ft_get_back(const t_vector vector);
+bool		ft_empty_vector(const t_vector vector);
+t_vector	ft_get_copy_vector(const t_vector	vector)
 
 #endif
