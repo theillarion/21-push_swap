b80d1ddd4708f14fc63e6752f0b6b93e95df3570
Illarion <pro100illarion@gmail.com>
2022-01-26 19:01:00 +0300

    Changed function

--- srcs/operations/validation.c
+++ srcs/operations/validation.c
@@ -1,15 +1,34 @@
 #include "operations.h"
 
-bool ft_validation(t_vector	*vector)
+t_vector	ft_get_copy_vector(const t_vector	vector)
 {
+	size_t		i;
+	t_vector	new_vector;
+
+	ft_initial_vector(&new_vector);
+	i = 0;
+	new_vector.elements = (int *)malloc(sizeof(int) * vector.count);
+	while (i < vector.count)
+	{
+		new_vector.elements[i] = vector.elements[i];
+		++i;
+	}
+	new_vector.count = vector.count;
+	return (new_vector);
+}
+
+bool ft_validation(const t_vector	vector)
+{
+	t_vector	vec;
 	size_t	i;
 	
-	if (!ft_sort(vector))
+	vec = ft_get_copy_vector(vector);
+	if (!ft_sort_vector(&vec))
 		return (false);
 	i = 0;
-	while (i < vector->count - 1)
+	while (i < vec.count - 1)
 	{
-		if (vector->elements[i] == vector->elements[i + 1])
+		if (vec.elements[i] == vec.elements[i + 1])
 			return (false);
 		++i;
 	}
