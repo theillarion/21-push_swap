f9091f5e9bf9679dabd2e2b1a4b157de8a575fd5
Illarion <pro100illarion@gmail.com>
2022-01-26 18:57:47 +0300

    Renamed file

--- srcs/operations/push.c
+++ srcs/operations/push.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_new.c                                         :+:      :+:    :+:   */
+/*   push.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: illarion <illarion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 20:48:27 by illarion          #+#    #+#             */
-/*   Updated: 2022/01/20 20:48:27 by illarion         ###   ########.fr       */
+/*   Updated: 2022/01/26 17:45:38 by illarion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "operations.h"
 
-void	ft_push_(t_stack	**src, t_stack	**dest)
+void	ft_push(t_stack	**src, t_stack	**dest, t_vector	*operations, int operation)
 {
 	t_stack	*backup;
 
@@ -20,4 +20,5 @@ void	ft_push_(t_stack	**src, t_stack	**dest)
 	*src = (*src)->next;
 	backup->next = *dest;
 	*dest = backup;
+	ft_push_back(operations, operation);
 }
