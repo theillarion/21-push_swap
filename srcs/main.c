4f98b3454e2f997222f233660ddb29d6fa233730
Illarion <pro100illarion@gmail.com>
2022-01-26 18:55:25 +0300

    Added new function and changed main function

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: illarion <illarion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 19:25:15 by illarion          #+#    #+#             */
-/*   Updated: 2022/01/25 00:01:25 by illarion         ###   ########.fr       */
+/*   Updated: 2022/01/26 18:42:26 by illarion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,25 +83,76 @@ void	ft_print_vector(const t_vector vector)
 	printf("\n");
 }
 
+void	ft_initial_main(t_main	*main_struct)
+{
+	main_struct->a = NULL;
+	main_struct->b = NULL;
+	ft_initial_vector(&main_struct->digits);
+	ft_initial_vector(&main_struct->operations);
+}
+
+void ft_print_operations(t_vector	operations)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < operations.count)
+	{
+		if (operations.elements[i] == sa)
+			write(1, "sa\n", 3);
+		else if (operations.elements[i] == sb)
+			write(1, "sb\n", 3);
+		else if (operations.elements[i] == ss)
+			write(1, "ss\n", 3);
+		else if (operations.elements[i] == pa)
+			write(1, "pa\n", 3);
+		else if (operations.elements[i] == pb)
+			write(1, "pb\n", 3);
+		else if (operations.elements[i] == ra)
+			write(1, "ra\n", 3);
+		else if (operations.elements[i] == rb)
+			write(1, "rb\n", 3);
+		else if (operations.elements[i] == rr)
+			write(1, "rr\n", 3);
+		else if (operations.elements[i] == rra)
+			write(1, "rra\n", 4);
+		else if (operations.elements[i] == rrb)
+			write(1, "rrb\n", 4);
+		else if (operations.elements[i] == rrr)
+			write(1, "rrr\n", 4);
+		++i;
+	}
+}
+
 int	main(int argc, char	**argv)
 {
-	t_vector	vector;
+	t_main	main_struct;
+	/*t_vector	vector;
 	t_stack		*a;
 	t_stack		*b;
 
 	(void)a;
 	(void)b;
 	(void)argc;
-	ft_initial(&vector);
+	ft_initial_vector(&vector);*/
+	ft_initial_main(&main_struct);
 	if (argc > 1)
 	{
-		ft_fill_vector(&vector, ++argv);
-		ft_print_vector(vector);
-		if (!ft_validation(&vector))
+		ft_fill_vector(&main_struct.digits, ++argv);
+		ft_print_vector(main_struct.digits);
+		if (!ft_validation(main_struct.digits))
 		{
 			write(1, "Error: not validation\n", 22);
 			exit(EXIT_FAILURE);
 		}
+		int	i = main_struct.digits.count - 1;
+		while (i >= 0)
+		{
+			ft_push_front(&main_struct.a, main_struct.digits.elements[i]);
+			--i;
+		}
+		ft_main_sort(&main_struct);
+		ft_print_operations(main_struct.operations);
 		exit(EXIT_SUCCESS);
 	}
 	else
