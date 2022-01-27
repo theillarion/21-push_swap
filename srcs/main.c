d6fc3329a1298b23457bc52590882a0e9b548c7a
Illarion <pro100illarion@gmail.com>
2022-01-27 20:21:38 +0300

    Refactored code

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: illarion <illarion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 19:25:15 by illarion          #+#    #+#             */
-/*   Updated: 2022/01/26 18:42:26 by illarion         ###   ########.fr       */
+/*   Updated: 2022/01/27 20:12:13 by illarion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,7 @@
 #include "libft.h"
 #include <stdio.h>
 
-void	ft_error(t_vector	*vector)
-{
-	ft_delete_vector(vector);
-	write(2, "Error\n", 6);
-	exit(EXIT_FAILURE);
-}
-
-bool	ft_chars_is_digit(const char	*str)
+static bool	ft_chars_is_digit(const char	*str)
 {
 	while (*str)
 	{
@@ -32,7 +25,7 @@ bool	ft_chars_is_digit(const char	*str)
 	return (true);
 }
 
-void	ft_fill_vector(t_vector	*vector,char	**argv)
+void	ft_fill_vector(t_main	*main_struct,char	**argv)
 {
 	size_t		i;
 	char		**buff;
@@ -45,8 +38,8 @@ void	ft_fill_vector(t_vector	*vector,char	**argv)
 		while (buff[i] != NULL)
 		{
 			if (!ft_chars_is_digit(buff[i]))
-				ft_error(vector);
-			ft_push_back(vector, ft_atoi(buff[i]));
+				ft_error(main_struct, "Error");
+			ft_push_back(&main_struct->digits, ft_atoi(buff[i]));
 			free(buff[i++]);
 		}
 		free(buff);
@@ -61,6 +54,8 @@ void	print_stack(t_stack	*stack)
 		printf("%d", ft_top(&stack));
 		ft_pop(&stack);
 		if (!ft_empty(&stack))
+			
+			
 			printf(" -> ");
 	}
 	printf("\n");
@@ -81,14 +76,6 @@ void	ft_print_vector(const t_vector vector)
 		++i;
 	}
 	printf("\n");
-}
-
-void	ft_initial_main(t_main	*main_struct)
-{
-	main_struct->a = NULL;
-	main_struct->b = NULL;
-	ft_initial_vector(&main_struct->digits);
-	ft_initial_vector(&main_struct->operations);
 }
 
 void ft_print_operations(t_vector	operations)
@@ -124,41 +111,36 @@ void ft_print_operations(t_vector	operations)
 	}
 }
 
+void	ft_fill_stack(t_main	*main_struct)
+{
+	int i;
+
+	i = main_struct->digits.count - 1;
+	while (i >= 0)
+	{
+		ft_push_front(&main_struct->a, main_struct->digits.elements[i]);
+		--i;
+	}
+}
+
 int	main(int argc, char	**argv)
 {
 	t_main	main_struct;
-	/*t_vector	vector;
-	t_stack		*a;
-	t_stack		*b;
 
-	(void)a;
-	(void)b;
-	(void)argc;
-	ft_initial_vector(&vector);*/
-	ft_initial_main(&main_struct);
+	ft_initial_struct(&main_struct);
 	if (argc > 1)
 	{
-		ft_fill_vector(&main_struct.digits, ++argv);
-		ft_print_vector(main_struct.digits);
+		ft_fill_vector(&main_struct, ++argv);
+		if (ft_is_sorted_vector(main_struct.digits))
+			ft_success(&main_struct);
 		if (!ft_validation(main_struct.digits))
-		{
-			write(1, "Error: not validation\n", 22);
-			exit(EXIT_FAILURE);
-		}
-		int	i = main_struct.digits.count - 1;
-		while (i >= 0)
-		{
-			ft_push_front(&main_struct.a, main_struct.digits.elements[i]);
-			--i;
-		}
+			ft_error(&main_struct, "Error");
+		ft_fill_stack(&main_struct);
 		ft_main_sort(&main_struct);
 		ft_print_operations(main_struct.operations);
+		ft_deinitial_struct(&main_struct);
 		exit(EXIT_SUCCESS);
 	}
 	else
-	{
-		/* ERROR */
-		write(1, "Error\n", 6);
 		exit(EXIT_FAILURE);
-	}
 }
