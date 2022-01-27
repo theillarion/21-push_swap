f694abee39b32464cebfe2647a9718f7923b0b32
Illarion <pro100illarion@gmail.com>
2022-01-27 20:17:00 +0300

    Changed prototypes functions and added new

--- includes/operations.h
+++ includes/operations.h
@@ -3,6 +3,8 @@
 
 # include "stack.h"
 # include "vector.h"
+# include "libft.h"
+# include <stdio.h>
 
 typedef struct s_main
 {
@@ -27,12 +29,19 @@ typedef enum e_operations
 	rrr = rra + rrb
 }	t_operations;
 
-void	ft_swap(t_stack	**head, t_vector	*operations, int operation);
-void	ft_push(t_stack	**src, t_stack	**dest, t_vector	*operations, int operation);
-void	ft_rotate(t_stack	**head, t_vector	*operations, int operation);
-void	ft_reverse_rotate(t_stack	**head, t_vector	*operations, int operation);
+void	ft_swap(t_stack	**head, t_vector	*operations, bool is_stack_a);
+void	ft_push(t_stack	**src, t_stack	**dest, t_vector	*operations, bool is_stack_a);
+void	ft_rotate(t_stack	**head, t_vector	*operations, bool is_stack_a);
+void	ft_reverse_rotate(t_stack	**head, t_vector	*operations, bool is_stack_a);
 bool	ft_validation(const t_vector vector);
+bool	ft_is_sorted_vector(const t_vector	vector);
 
 void	ft_main_sort(t_main	*main_struct);
+
+void	ft_initial_struct(t_main	*main_struct);
+void	ft_deinitial_struct(t_main	*main_struct);
+void	ft_error(t_main	*main_struct, char	*message);
+void	ft_success(t_main	*main_struct);
+
 
 #endif
