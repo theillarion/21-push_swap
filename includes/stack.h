ce0f00639dc9d85ab59aecd0988df20f5a73da38
Illarion <pro100illarion@gmail.com>
2022-01-28 01:46:18 +0300

    Function prototype arguments changed

--- includes/stack.h
+++ includes/stack.h
@@ -1,9 +1,10 @@
 #ifndef STACK_H
 # define STACK_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdbool.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdbool.h>
+# include <limits.h>
 
 typedef struct s_stack
 {
@@ -12,10 +13,10 @@ typedef struct s_stack
 }					t_stack;
 
 void	ft_pop(t_stack	**head);
-int		ft_top(t_stack	**head);
-bool	ft_empty(t_stack	**head);
+int		ft_top(const t_stack	*head);
+bool	ft_empty(const t_stack	*head);
 void	ft_push_front(t_stack	**head, int data);
 void	ft_push_array(t_stack	**head, int	*array, size_t	length);
-size_t	ft_size(t_stack	**head);
+size_t	ft_size(const t_stack	*head);
 
 #endif
