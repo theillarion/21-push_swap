55c09d54d84625997afdb6f31e602075ba704d61
Glenn Ashli <glashli@un-e2.kzn.21-school.ru>
2022-02-03 16:14:56 +0300

    Added new prototype functions

--- includes/checker.h
+++ includes/checker.h
@@ -4,6 +4,8 @@
 # include "shared.h"
 # include "get_next_line.h"
 
-int	main(int argc, char	**argv);
+int		main(int argc, char	**argv);
+void	ft_fill_operations(t_main	*main_struct);
+bool	ft_check(t_main	*main_struct);
 
 #endif
