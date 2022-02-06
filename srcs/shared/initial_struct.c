70761d43d05b03130961cb2b4225d0721a404fb6
Illarion <pro100illarion@gmail.com>
2022-02-06 14:29:23 +0300

    Fixed leaks

--- srcs/shared/initial_struct.c
+++ srcs/shared/initial_struct.c
@@ -5,6 +5,7 @@ void	ft_initial_struct(t_main	*main_struct)
 	main_struct->a = NULL;
 	main_struct->b = NULL;
 	ft_initial_vector(&main_struct->vector);
+	ft_initial_vector(&main_struct->sorted_vector);
 	ft_initial_vector(&main_struct->operations);
 }
 
@@ -15,6 +16,7 @@ void	ft_deinitial_struct(t_main	*main_struct)
 	while (!ft_empty(main_struct->b))
 		ft_pop(&main_struct->b);
 	ft_delete_vector(&main_struct->vector);
+	ft_delete_vector(&main_struct->sorted_vector);
 	ft_delete_vector(&main_struct->operations);
 }
 
