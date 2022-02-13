4cefb1c58fa0abe96ba1802873986b6b2995f184
Illarion <pro100illarion@gmail.com>
2022-02-13 20:57:07 +0300

    Moxed prototype functions to push_swap header file

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -1,7 +1,7 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-# include "sort.h"
+# include "shared.h"
 //	FORBIDDEN	HEADER
 # include "forbidden_func.h"
 
@@ -12,5 +12,23 @@ int		ft_get_max(const t_stack	*head);
 int		ft_get_min(const t_stack	*head);
 
 void	ft_print_operations(t_vector	operations);
+
+void	ft_main_sort(t_main	*main_struct);
+
+void	ft_sort_mini(t_main	*main_struct);
+void	ft_sort_2(t_stack	**stack, t_vector	*operations, bool is_stack_a);
+void	ft_sort_3(t_stack	**stack, t_vector	*operations, bool is_stack_a);
+
+void	ft_sort_full(t_main	*main_struct);
+void	ft_presort(t_main	*_main);
+void	ft_do_operations(t_main	*_main);
+void	ft_set_score(t_main	*_main);
+void	ft_finaly_rotate(t_main	*_main);
+
+bool	ft_is_non_strict_sorted(const t_vector vector, const t_value value);
+void 	ft_non_strict_sort(t_main	*main);
+
+bool	ft_is_reverse_non_strict_sorted(const t_vector vector, const t_value value);
+void	ft_reverse_non_strict_sort(t_main	*main);
 
 #endif
